d3cc1dd6d911817e9b7753ae0b25c1cfe46011b2
Kateryna Ivaniuk <katherina.ivaniuk@gmail.com>
2024-10-31 11:40:30 -0700

    Created the first section of the Projects Page

--- frontend/src/components/Projects/Projects.js
+++ frontend/src/components/Projects/Projects.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import './Projects.css';
 
 function Projects() {
-    return (
-        <div>
-            <h1>Projects Page</h1>
-            <p>Information about our projects will be displayed here.</p>
+  return (
+    <section className="projects-page-content">
+      <div className="projects-page-intro">
+        <div className="projects-intro-text">
+          <h1 className="projects-heading">
+            <span className="projects-heading-line1">One Map,</span>
+            <span className="projects-heading-line2">Many Uses</span>
+          </h1>
+          <p className="projects-description">
+            Use the map below to explore the regions impacted by the full-scale invasion. By clicking on a pin, you can learn about the reconstruction project in that region, its history, level of urgency, and the estimated rebuilding cost.
+          </p>
+          <button className="projects-support-button">#SUPPORTUKRAINE 💙💛</button>
         </div>
-    );
+        <div className="projects-intro-image">
+          <img src="/images/Flag.png" alt="Independence Statue" />
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Projects;
